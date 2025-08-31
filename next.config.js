b23d98b0554ec9bb098e1e3131d8fff58b040208
nikhiluday <nuday_be20@thapar.edu>
2025-09-01 03:39:50 +0530

    c

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // You can add other config options here
+};
+
+module.exports = nextConfig;

c4454cd316181765ccb3b93d823514a6b059a282
nikhiluday <nuday_be20@thapar.edu>
2025-09-01 12:45:02 +0530

    cloudflare edge issue

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ContactFormResponse } from '../../types';
 import nodemailer from 'nodemailer';
+export const runtime = 'edge';
 
 export default async function handler(
   req: NextApiRequest,
